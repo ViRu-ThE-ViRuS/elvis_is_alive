2ce102ebe0aa49d5ea5a86c7e11c2433496e9abf
Viraat Chandra <viraat.chandra@hotmail.com>
2021-06-05 22:22:52 +0530

    fix sarsa, add nstep dqn

--- ndqn.py
+++ ndqn.py
@@ -1,16 +1,37 @@
 import torch as T
 import torch.nn as nn
 import torch.nn.functional as F
-
 from torchviz import make_dot
 
 import gym
 import numpy as np
+from collections import deque
 
 
-class DeepSarsaAgent(nn.Module):
-    def __init__(self, input_shape, output_shape, hidden_layer_dims, update_step=8):
-        super(DeepSarsaAgent, self).__init__()
+class ReplayBuffer:
+    def __init__(self, mem_size):
+        self.mem_size = mem_size
+        self.buffer = deque(maxlen=mem_size)
+
+    def sample(self, batch_size):
+        sample_size = min(batch_size, len(self.buffer))
+        sample_indices = np.random.choice(len(self.buffer), sample_size)
+        samples = np.array(self.buffer, dtype=object)[sample_indices]
+        return map(list, zip(*samples))
+
+    def get_all(self, reverse=True):
+        return map(list, zip(*reversed(self.buffer))) if reverse else map(list, zip(*self.buffer))
+
+    def store(self, transition):
+        self.buffer.append(transition)
+
+    def clear(self):
+        self.buffer = deque(maxlen=self.mem_size)
+
+
+class DeepQN(nn.Module):
+    def __init__(self, input_shape, output_shape, hidden_layer_dims):
+        super(DeepQN, self).__init__()
 
         layers = []
         layers.append(nn.Linear(*input_shape, hidden_layer_dims[0]))
@@ -22,23 +43,17 @@ class DeepSarsaAgent(nn.Module):
         self.loss = nn.MSELoss()
         self.optimizer = T.optim.Adam(self.parameters(), lr=0.001)
 
-        self.update_step = 8
-        self.update_counter = 0
-
     def forward(self, states):
         for layer in self.layers[:-1]:
             states = F.relu(layer(states))
         return self.layers[-1](states)
 
     def learn(self, predictions, targets):
-        self.update_counter += 1
         loss = self.loss(input=predictions, target=targets)
-        loss.backward(retain_graph=True)
 
-        if self.update_counter % self.update_step == 0:
-            self.optimizer.step()
-            self.optimizer.zero_grad()
-
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
         return loss
 
 
@@ -47,13 +62,16 @@ class Agent:
         self.epsilon = epsilon
         self.gamma = gamma
         self.output_shape = output_shape
+        self.n_step = 16
 
-        self.q_eval = DeepSarsaAgent(input_shape, output_shape, [64, 64])
-        self.q_target = DeepSarsaAgent(input_shape, output_shape, [64, 64])
+        self.q_eval = DeepQN(input_shape, output_shape, [64, 64])
+        self.q_target = DeepQN(input_shape, output_shape, [64, 64])
+        self.memory = ReplayBuffer(self.n_step)
 
-        self.tau = 32
-        self.update_step = 8
+        self.tau = 8
+        self.batch_size = 32
         self.learn_step = 0
+
         self.update()
 
     def move(self, state):
@@ -70,18 +88,36 @@ class Agent:
             self.q_target.load_state_dict(self.q_eval.state_dict())
             self.q_target.eval()
 
+    def get_all(self):
+        (actions, states, states_, rewards, terminals) = self.memory.get_all(reverse=True)
+        self.memory.clear()
+
+        discounted_rewards = np.zeros(len(rewards),)
+        _reward = 0
+        for index, (reward, terminal) in enumerate(zip(rewards, terminals)):
+            _reward = discounted_rewards[len(rewards) - index - 1] = reward * (1 - terminal) + self.gamma * _reward
+
+        actions = T.tensor(actions).long()
+        states = T.tensor(states).float()
+        states_ = T.tensor(states_).float()
+        rewards = T.tensor(discounted_rewards).float()
+        terminals = T.tensor(terminals).long()
+
+        return actions, states, states_, rewards, terminals
+
     def learn(self, state, action, state_, reward, done):
         self.learn_step += 1
-        action = T.tensor(action).long()
-        state = T.tensor(state).float()
-        state_ = T.tensor(state_).float()
-        reward = T.tensor(reward).float()
-        terminal = T.tensor(done).long()
+        self.memory.store((action, state, state_, reward, done))
+
+        if self.learn_step % self.n_step:
+            return
 
         self.q_eval.train()
-        q_eval = self.q_eval(state)[action]
-        q_next = self.q_target(state_).detach().max(axis=0)[0]
-        q_target = reward + self.gamma * q_next * (1 - terminal)
+        actions, states, states_, rewards, terminals = self.get_all()
+        indices = np.arange(len(actions))
+        q_eval = self.q_eval(states)[indices, actions]
+        q_target = self.q_target(states_).detach().max(axis=1)[0]
+        q_target = rewards + self.gamma * q_target * (1 - terminals)
 
         loss = self.q_eval.learn(q_eval, q_target)
         self.epsilon *= 0.95 if self.epsilon > 0.1 else 1.0
@@ -135,6 +171,7 @@ def learn(env, agent, episodes=500):
 
 if __name__ == '__main__':
     env = gym.make('CartPole-v1')
+    # env = gym.make('LunarLander-v2')
     agent = Agent(1.0, 0.9, env.observation_space.shape, [env.action_space.n])
 
-    learn(env, agent, 1000)
+    learn(env, agent, 5000)
